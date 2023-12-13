0780e299587980a0acd4d8094752e6f908e523e8
im-vishal <iamvirtualvishal@gmail.com>
2023-12-13 21:30:04 +0530

    adding rest cookiecutter files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project leverages advanced machine learning techniques to build a robust credit card fraud detection system. The combination of Cookiecutter for project scaffolding and DVC (Data Version Control) for managing and versioning machine learning data and models ensures a streamlined and reproducible workflow.',
+    author='Vishal Pandey',
+    license='',
+)
